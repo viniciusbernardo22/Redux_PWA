472b3709030d0c8f064fb054d9c7edf2e9382199
Vinicius <vini383@gmail.com>
2023-11-09 16:05:56 -0300

    feat: created middleware

---
+++ src/store/middleware/logger.ts
@@ -0,0 +1,10 @@
+import { Middleware, Dispatch, AnyAction, Store } from 'redux';
+
+const logger: Middleware<{}, any, Dispatch<AnyAction>> = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log('ACTION', action);
+  console.log('PREV_STATE', store.getState());
+  const result = next(action);
+  console.log('NEW_STATE', store.getState());
+  return result;
+};
