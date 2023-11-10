e602a7c306616387b22c478943fb587811900971
Vinicius <vini383@gmail.com>
2023-11-10 12:06:01 -0300

    feat: added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const {contador} = useSelector<State, any>((state) => state);
 
   return (
-    <div>
+    <div >
       <h1>Total: {contador}</h1>
       <button onClick={() => dispatch(incrementar())}>Incrementar</button>
       <button onClick={() => dispatch(reduzir())}>Reduzir</button>
