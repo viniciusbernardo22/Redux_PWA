8313334128f0b58cc468833402e56178785cf2e0
Vinicius <vini383@gmail.com>
2023-11-11 13:07:13 -0300

    feat: logger rebuilt

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -7,9 +7,9 @@ import contador from './contador';
 import modal from './modal';
 import login from './login';
 
-// import logger from './middleware/logger';
+ import logger from './middleware/logger';
 
-const middleware = [...getDefaultMiddleware()];
+const middleware = [...getDefaultMiddleware(), logger];
 const reducer = combineReducers({ contador, modal, login });
 const store = configureStore({ reducer, middleware });
 
